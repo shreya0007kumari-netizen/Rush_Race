1d49a3f523ff4a4370ca8d8cf610d7f95b55b130
shreya0007kumari-netizen <shreya.0007.kumari@gmail.com>
2026-09-05 21:37:15 +0530

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.rushrace.game',
+  appName: 'RUSH Race',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
